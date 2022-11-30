a56a60d28a797dd11f780ba64f212fda60f6de61
Vitaly <vitalik.emperor@gmail.com>
2022-11-30 10:14:37 +0500

    Added solution 2 tasks

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+//Проверяем, чтобы данные не были пустыми
+if (num1Line!=null && num2Line!=null)
+{
+    //Парсим первое число
+    int num1 = int.Parse(num1Line);
+    //Парсим второе число
+    int num2 = int.Parse(num2Line);
+    //Даем условие
+    if (num1 < num2)
+    {
+        //Выводим данные в консоль
+        Console.Write("max = "); 
+        Console.WriteLine(num2Line);
+    }
+    else
+    {
+        //Выводим данные в консоль
+        Console.Write("max = "); 
+        Console.WriteLine(num1Line);
+    }
+}
