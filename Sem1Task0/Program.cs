9cabddd38639ca9f57fe7f4239b5b7bd8777f0a5
Vitaly <vitalik.emperor@gmail.com>
2022-11-30 05:55:31 +0500

    Added 1 algorithm

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+if(num1Line!=null&&num2Line!=null)
+{
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    int outResult = num1*num2;
+    Console.WriteLine(outResult);
+}
