841174cf1e446d1dcf51a8d929dcf40433cb4d86
Vitaly <vitalik.emperor@gmail.com>
2022-12-02 01:52:37 +0500

    Added solutions 6, 8 tasks

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Считываем данные с консоли
+string? inputNum = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if (inputNum != null)
+{
+    //Парсим число
+    int num = int.Parse(inputNum);
+
+    //Условие: число делится на 2 без остатка
+    if (num % 2 == 0)
+    {
+        //Выводим в консоль "Число является четным"
+        Console.Write("Number is even");
+    }
+    else 
+    {
+        //Выводим в консоль "Число не является четным"
+        Console.Write("Number is not even");
+    }
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,31 @@
+﻿//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(inputLine!=null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+    //Вводим дополнительную переменную
+    int startNumber = 1;
+    //Вводим переменную, в которой накапливаем результат
+    string outLine = string.Empty;
+    //Создаем цикл
+    while(startNumber < inputNumber)
+    {
+        if (startNumber % 2 == 0)
+        {
+        Console.Write(startNumber);
+        }
+        //startNumber = startNumber+1;
+        startNumber++;
+    }
+    //Добавляем начальное число
+    outLine = outLine + inputNumber;
+    //Условие: начальное число будет выводиться, только если оно четное 
+    if (inputNumber % 2 == 0)
+    {
+        //Выводим данные в консоль
+        Console.Write(inputNumber);
+    }
+}   
