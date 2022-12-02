a8f1e4d0346884c0f3411539b175afe439f4bff3
Vitaly <vitalik.emperor@gmail.com>
2022-12-03 00:11:41 +0500

    Added solutions 9, 11, 12, 14, 16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();   
+
+// получаем случайное число
+int num = numberSintezator.Next(100,1000);             
+Console.WriteLine (num);                         
+
+int result = (num/100)*10 + num%10;
+Console.WriteLine (result);
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Random numberSintezator = new System.Random();
+
+int number = numberSintezator.Next(10,100);
+
+Console.WriteLine(number);
+
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+if (firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
+
+//Вариант 2
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+
+firstNumber = ((int)digits[0])-48;
+secondNumber = ((int)digits[1])-48;
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+
+Console.WriteLine(resultNumber);
