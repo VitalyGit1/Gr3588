1d52bf3e3d732e27da36af33098c8224151bf376
Vitaly <vitalik.emperor@gmail.com>
2022-12-08 22:12:45 +0500

    Added solutions 19, 21, 23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int num = readData("Введите пятизначный палиндром:");
+
+if (num == 0)
+{
+    return;
+}
+
+bool result = palinTest(num);
+
+printData(result);
+
+int readData(string msg)
+{
+    Console.WriteLine(msg);
+
+    string? inputNumber = Console.ReadLine();
+
+    if (inputNumber.Length != 5)
+    {
+        Console.WriteLine("Число не является пятизначным!");
+        return 0;
+    }
+    return int.Parse(inputNumber ?? "0");
+}
+
+bool palinTest(int num)
+{
+    bool result = false;
+    result = (num / 10000 == num % 10) && (num / 1000) % 10 == (num / 10) % 10;
+    return result;
+}
+
+void printData(bool result)
+{
+    if (result == true)
+    {
+        Console.WriteLine("Введенное число - палиндром!");
+    }
+
+    else
+    {
+        Console.WriteLine("Введенное число - не палиндром!");
+    }
+}
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int x1 = ReadData("Введите x1: ");
+int x2 = ReadData("Введите x2: ");
+int y1 = ReadData("Введите y1: ");
+int y2 = ReadData("Введите y2: ");
+int z1 = ReadData("Введите z1: ");
+int z2 = ReadData("Введите z2: ");
+
+double CalculateLength(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double res = 0;
+    res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+    return res;
+}
+
+
+void PrintData(string msg, double result)
+{
+    Console.WriteLine(msg + result);
+}
+
+PrintData("Расстояние между точками ", CalculateLength(x1, x2, y1, y2, z1, z2));
+
