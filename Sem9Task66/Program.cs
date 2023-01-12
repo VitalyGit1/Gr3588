799d5500117606efa15550a86d1451e7519cc7c3
Vitaly <vitalik.emperor@gmail.com>
2023-01-13 00:52:55 +0500

    Added solutions 64, 66, 68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = ReadData("Enter number M: ");
+int N = ReadData("Enter number N: ");
+int sum = SumMNRec(M, N);
+PrintData("The sum of natural elements between M and N: " + sum);
+
+int ReadData(string line) // Read data from user.
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintData(string res) // Print data to console.
+{
+    Console.WriteLine(res);
+}
+
+int SumMNRec(int M, int N) // Find the sum of natural elements from M to N using recursion.
+{
+    if (M >= N) return N;
+    return M + SumMNRec(M + 1, N);
+}
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int M = ReadData("Enter number M: ");
+int N = ReadData("Enter number N: ");
+int elem = AkkermanFunc(M, N);
+PrintData("Calculations: " + elem);
+PrintData("Calculations: " + AkkermanFunc(3,2));
+
+int ReadData(string line) // Read data from user.
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintData(string res) // Print data to console.
+{
+    Console.WriteLine(res);
+}
+
+int AkkermanFunc(int m, int n) // Ackerman function calculation program using recursion.
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return AkkermanFunc(m - 1, 1);
+    }
+    return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+}
+
+
+
